540c0e8f18c7197014259e785153668ce9e65e4e
kyyeto9984 <kyyeto9984@naver.com>
2020-09-10 12:21:52 +0900

    Initial commit

---
+++ src/test/java/me/whiteship/springbootmvc/SpringbootmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.springbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
